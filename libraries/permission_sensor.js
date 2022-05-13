6554cd6e44ecc6c06ede8e563d472c99305b8887
Magnus <Magnusvjensen@gmail.com>
2022-05-13 09:25:58 +0200

    fixing small bug

--- libraries/permission_sensor.js
+++ libraries/permission_sensor.js
@@ -1,7 +1,7 @@
 function touchStarted()
 {
   document.querySelector("h1").style.display = "none"
-  document.querySelector("h2").style.display = "none"
+  document.querySelector("h3").style.display = "none"
 
 
   if (typeof DeviceMotionEvent.requestPermission === 'function') 
